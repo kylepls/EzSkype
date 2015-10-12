deb16ab3a539e3dda43b066d97fb6bafb12c1f1e
Kyle <kylemock@rocketmail.com>
2015-10-12 09:28:51 -0600

    Update empty convo

--- src/main/lombok/in/kyle/ezskypeezlife/internal/obj/SkypeConversationInternalEmpty.java
+++ src/main/lombok/in/kyle/ezskypeezlife/internal/obj/SkypeConversationInternalEmpty.java
@@ -2,6 +2,7 @@ package in.kyle.ezskypeezlife.internal.obj;
 
 import in.kyle.ezskypeezlife.EzSkype;
 import in.kyle.ezskypeezlife.api.SkypeConversationType;
+import in.kyle.ezskypeezlife.api.obj.SkypeUser;
 
 import java.util.Collections;
 
@@ -12,5 +13,15 @@ public class SkypeConversationInternalEmpty extends SkypeConversationInternal {
     
     public SkypeConversationInternalEmpty(EzSkype ezSkype, String longId) {
         super(ezSkype, longId, "", false, false, null, SkypeConversationType.GROUP, Collections.EMPTY_LIST);
+    }
+    
+    @Override
+    public boolean kick(SkypeUser skypeUser) {
+        return false;
+    }
+    
+    @Override
+    public boolean isAdmin(SkypeUser skypeUser) {
+        return false;
     }
 }
